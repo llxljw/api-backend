d0706bab3eafa75fd6a3783b50a5cd6757b3a520
ljw <1467920854@qq.com>
2023-06-25 22:10:20 +0800

    管理员发布，下线，在线调试接口；补全接口调用需要的参数

---
+++ src/main/java/com/yupi/springbootinit/mapper/InterfaceInfoMapper.java
@@ -0,0 +1,19 @@
+package com.yupi.springbootinit.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yupi.springbootinit.model.entity.InterfaceInfo;
+
+
+/**
+* @author 李建伟llx
+* @description 针对表【interface_info(接口信息)】的数据库操作Mapper
+* @createDate 2023-06-25 21:53:42
+* @Entity generator.domain.InterfaceInfo
+*/
+public interface InterfaceInfoMapper extends BaseMapper<InterfaceInfo> {
+
+}
+
+
+
+
---
+++ src/main/java/generator/service/InterfaceInfoService.java
@@ -0,0 +1,13 @@
+package generator.service;
+
+import generator.domain.InterfaceInfo;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author 李建伟llx
+* @description 针对表【interface_info(接口信息)】的数据库操作Service
+* @createDate 2023-06-25 21:53:42
+*/
+public interface InterfaceInfoService extends IService<InterfaceInfo> {
+
+}
